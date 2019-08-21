c9dd3a7395972da29047dc912ed197246635fd95
Adam Petherick <a_petherick@hotmail.com>
2019-08-21 20:17:53 +1200

    adding subtract test Testcalc

--- src/test/java/Tut4/Testcalc.java
+++ src/test/java/Tut4/Testcalc.java
@@ -4,8 +4,12 @@ public class Testcalc
    
 {
 
-    public void testApp()
+    public void testAdd()
     {
         assert( 333 == (new Calc().add(111,222)) );
+    }
+	public void testSubtract()
+    {
+        assert( 111 == (new Calc().subtract(333,222)) );
     }
 }
